cc44092cf6fb29de507087cc9e55d386b605de89
TimCooley <156612347+TimCooley@users.noreply.github.com>
2024-01-30 20:27:03 -0500

    Update CDR Report (Weekly & Monthly).sql

--- CDR Report (Weekly & Monthly).sql
+++ CDR Report (Weekly & Monthly).sql
@@ -8,11 +8,7 @@
 
 SELECT DISTINCT CM.CASE_ID,C.COMPLAINT_SHORT_NAME,CTL.COMPLAINT_TYPE, C.PARTICIPATION_TYPE
 FROM CASE_MASTER.case_complaint_mapping CM, CASE_MASTER.COMPLAINT_TYPE_LOOKUP CTL, CASE_MASTER.COMPLAINT C
-WHERE CM.CASE_ID IN ( 
-
-24045, 18896, 17258, 19593, 24324, 23116, 24441, 17268, 21744, 23020, 24472, 21637, 23954, 23212, 20026, 21532, 24444, 24612, 17316, 22386, 17866, 24279, 19049, 24874, 17049, 21392, 22624, 18849, 24914, 25014, 24712, 19211, 22563, 24979, 22934, 21939, 20831, 18672, 20806, 24953, 25015, 25012, 25020, 24443, 24274, 25038, 24815, 19008, 19486, 25023, 19683, 25093, 25052, 25043, 25056, 25232, 24835, 19339, 25192, 22601, 25115, 18608, 22610, 24063, 25114, 22914, 17368, 23065, 21749, 23881, 25272, 20360, 20274, 20479, 25036, 17365, 24044, 25472, 25152, 24294, 18385, 16448, 9766, 15238, 17733, 23778, 25053, 23893, 23472, 20885, 25055, 23064, 21150, 20366, 21678, 25372, 22944, 25413, 20080, 22339, 20611, 25588, 19968, 17764, 19943, 23066, 22275, 25092, 20003, 24278, 21452, 17287, 17410, 25512, 11479, 25652, 15835, 24160, 25173, 16639, 16988, 25516, 23125, 25533, 17535, 23713, 22365, 16699, 25712, 22174, 25733, 25432, 22102, 23017, 22536, 25715, 19226, 25808, 25714, 25717, 25492, 20653, 22158, 20789, 24277, 23880, 25242, 25878, 23541, 22599, 25837, 20297, 25892, 25914, 24434, 22814, 23733, 22874, 22310, 25916, 19352, 24536, 25932, 24148, 25915, 24142, 18867, 24195, 22472, 24220, 23572, 25972, 23897, 25973, 20603, 21338, 24140, 21644, 26195, 26094, 26012, 25936, 20980, 26253, 25573, 26173, 23284, 24114, 22774, 26292, 25745, 26212, 20961, 24046, 18547, 26352, 23800, 26293, 24159
-
- )
+WHERE CM.CASE_ID IN ( 24045, 18896, 17258, 19593)
 AND CTL.COMPLAINT_TYPE_ID = C.COMPLAINT_TYPE_ID
 AND CM.COMPLAINT_ID = C.COMPLAINT_ID
 ;
@@ -22,7 +18,7 @@ AND CM.COMPLAINT_ID = C.COMPLAINT_ID
 
 select *
 from (
-Select  C.case_ID, C.CASE_NAME,C.FILING_CLAIM_DEADLINE_DATE,Cc.processed_date,CC.CASE_STATUS,CC.PARTICIPATION_STATUS, rank() over (partition by CC.case_id order by cC.processed_date desc) rnk
+Select  C.case_ID, C.CASE_NAME,C.FILING_CLAIM_DEADLINE_DATE,CC.processed_date,CC.CASE_STATUS,CC.PARTICIPATION_STATUS, rank() over (partition by CC.case_id order by cC.processed_date desc) rnk
 FROM CASE C, CASE_MASTER.CASE_CHANGES CC
 --WHERE CASE_ID = &CASE_ID_VAR
 where C.case_name like ('%Master Case%')
@@ -44,7 +40,7 @@ where rnk = 1
 Select case_ID, CASE_NAME,FILING_CLAIM_DEADLINE_DATE, PARTICIPATION
 FROM CASE
 --WHERE FILING_CLAIM_DEADLINE_DATE >= round (SYSDATE)-- and FILING_CLAIM_DEADLINE_DATE <= ROUND (SYSDATE) +90
---WHERE (CASE_ID IN (25236, 20822, 23513, 24998, 15853, 21096, 25076, 15718, 18432, 455, 16956, 19012, 23088, 17867, 18006, 24832, 23274, 23276, 25034, 20998, 23152, 20780, 22330, 17541, 24286, 10948, 24097, 19162, 20803, 25292, 22382, 19296, 9701, 20811, 20188, 20585, 22894, 16233, 20550, 19853, 23702, 17267, 11152, 24276, 21248, 17462, 10839, 22508, 11176, 16405, 19629, 16999, 22507, 11437, 21523, 20765, 16237, 17352, 17542, 15854, 10719, 20737, 16387, 20582, 23162, 19898, 19773, 22958, 19902, 22993, 10754, 19161, 24693, 19846, 20210, 24838, 25018, 16576, 18075, 11444, 20767, 18563, 23293, 18431, 16512, 21195, 15672, 9700, 22453, 22605, 24402, 25235, 23741, 16049, 25019, 1095, 19013, 23328, 22941, 21345, 17252, 25273, 16498, 23092, 21346, 19631, 21941, 17084, 23700, 24099, 23054, 23472, 17495, 23995, 25035, 23719, 23393, 15325, 16194, 24822, 16014, 20583, 6026, 23803, 25454, 15936, 10089, 18155, 23495, 20057, 15852, 21348, 10468, 22948))
+--WHERE (CASE_ID IN (25236, 20822, 23513, 24998, 15853))
 ORDER BY FILING_CLAIM_DEADLINE_DATE
 ;
 
@@ -146,7 +142,7 @@ from jbrown.jira_tasks JT,jbrown.JIRA_TASK_CASE_ID CI
 where project_name in ('Case Data Request')
 AND STATUS NOT IN ( 'Done')
 and client is not null
-AND (ASSIGNEE IN ('Kenneth Thompson', 'Thomas Hobson', 'Ryan Kingsbury','Hari Meka','Chris Barber')) --- DI TEAM
+AND (ASSIGNEE IN ('******)) --- DI TEAM
 AND CI.CASE_ID NOT IN (25516,25092)--------------FF
 and JT.ID = CI.TASK_ID
 order by created
@@ -159,8 +155,8 @@ select COUNT (*)
 from jbrown.jira_tasks
 where project_name in ('Case Data Request')
 and client is not null
---AND (ASSIGNEE IN ('Tim Cooley','Elijah Barnes', 'Gabriel Slama', 'andrewtran', 'Stephano Barrios', 'Marvin Perez'))-- ETL TEAM
-AND (ASSIGNEE IN ('Kenneth Thompson', 'Thomas Hobson','Ryan Kingsbury','Hari Meka','Chris Barber')) --- DI TEAM
+--AND (ASSIGNEE IN (*****))-- ETL TEAM
+AND (ASSIGNEE IN (******)) --- DI TEAM
 AND STATUS = 'To Do'
 --AND DUE_DATE <= SYSDATE - 1
 ;
@@ -175,7 +171,7 @@ select KEY, CASE_ID, summary,CLAIM_DEADLINE, ASSIGNEE, STATUS, due_date,round (s
 from jbrown.jira_tasks JT,jbrown.JIRA_TASK_CASE_ID CI
 where project_name in ('Case Data Request')
 and client is not null
-AND (ASSIGNEE IN ('Kenneth Thompson', 'Thomas Hobson', 'Ryan Kingsbury','Hari Meka','Chris Barber')) --- DI TEAM
+AND (ASSIGNEE IN (******)) --- DI TEAM
 AND STATUS = 'To Do'
 AND (DUE_DATE <= SYSDATE)-- or DUE_DATE >= SYSDATE )
 and JT.ID = CI.TASK_ID
@@ -208,7 +204,7 @@ select key,case_id,summary,claim_deadline,assignee,status, due_date, round (sysd
 from jbrown.jira_tasks JT,jbrown.JIRA_TASK_CASE_ID CI
 where project_name in ('Case Data Request')
 and client is not null
-AND (ASSIGNEE IN ('Kenneth Thompson', 'Thomas Hobson', 'Ryan Kingsbury','Hari Meka','Chris Barber')) --- DI TEAM
+AND (ASSIGNEE IN (******)) --- DI TEAM
 AND STATUS not in 'Done'
 AND (CLAIM_DEADLINE <= SYSDATE + 14 and CLAIM_DEADLINE >= SYSDATE -1)
 and JT.ID = CI.TASK_ID
@@ -236,7 +232,7 @@ select key, CASE_ID,summary,claim_deadline,assignee,status, due_date, round (sys
 from jbrown.jira_tasks JT,jbrown.JIRA_TASK_CASE_ID CI
 where project_name in ('Case Data Request')
 AND STATUS NOT IN ( 'Done')
-AND (ASSIGNEE IN ('Kenneth Thompson', 'Thomas Hobson', 'Ryan Kingsbury','Hari Meka','Chris Barber')) --- DI TEAM
+AND (ASSIGNEE IN (******)) --- DI TEAM
 AND CI.CASE_ID IN (
 select CASE_ID
 from (
@@ -264,7 +260,7 @@ from jbrown.jira_tasks JT,jbrown.JIRA_TASK_CASE_ID CI
 where project_name in ('Case Data Request')
 AND STATUS NOT IN ( 'Done')
 and client is not null
-AND (ASSIGNEE IN ('Kenneth Thompson', 'Thomas Hobson', 'Ryan Kingsbury','Hari Meka','Chris Barber')) --- DI TEAM
+AND (ASSIGNEE IN (******)) --- DI TEAM
 AND CI.CASE_ID IN (25573)--------------FF
 and JT.ID = CI.TASK_ID
 order by created
@@ -376,7 +372,7 @@ order BY FIRST_UPCOMING_CLAIM_FILING_DEADLINE
 select RN.CLIENT_NAME, /**//*RN.ACCOUNT_ID,*//* */COUNT (ACCOUNT_ID) AS "ACCOUNT_IDS_REQUESTED"
 from case_specialists.RECON_NOTES RN
 WHERE RN.CDR_CREATED = 'Y'
---and CLIENT_NAME IN ('BLACKROCK')
+--and CLIENT_NAME IN ('******')
 and COMPLETED_DATE > '01-JAN-2024'
 AND CASE_ID NOT IN (25573,25092,25516) ---- FF
 GROUP BY RN.CLIENT_NAME/*,RN.ACCOUNT_ID*/
@@ -389,7 +385,7 @@ ORDER BY ACCOUNT_IDS_REQUESTED DESC
 SELECT CLIENT_NAME, PRODUCT_TYPE, CASE_ID, COUNT ( ACCOUNT_ID) AS "ACCTS_IN_CDR", TRUNC(CREATE_DATE) AS "CREATE_DATE" 
 FROM case_specialists.RECON_NOTES RN
 WHERE RN.CDR_CREATED = 'Y'
---AND CLIENT_NAME IN ('BLACKROCK')
+--AND CLIENT_NAME IN ('******')
 AND RECON_ID IS NOT NULL AND PRODUCT_TYPE IS NOT NULL AND CREATE_DATE IS NOT NULL
 and COMPLETED_DATE > '01-JAN-2024'
 AND CASE_ID NOT IN (25573,25092,25516) ---- FF
@@ -404,7 +400,7 @@ ORDER BY TRUNC(CREATE_DATE) DESC
 
 ------------------------------------- 3. COUNT OF CDRS MADE PER ACCOUNT_NUMBER YTD
 -- ADD DATA PROVIDER FOR EACH ACCOUNT NUMBER
--- P 12345 ACCT NUMBER FORMAT = JPM CUSTODIAN 
+
 
 WITH A AS(
 SELECT DISTINCT RN.CLIENT_NAME, RN.CASE_ID,RN.ACCOUNT_ID, AC.CLIENT_REF_NUMBER/*,A.NAME*/
@@ -415,23 +411,23 @@ and RN.COMPLETED_DATE > '01-JAN-2024'
 ),
 B AS(
 SELECT  A.CLIENT_NAME, AC.CLIENT_REF_NUMBER, /*AC.NAME, */
-case when ((ac.name like '%INTERNAL%' or ac.name like '%Internal%') and ac.name not in ('INTERNAL_STATE_STREET')) or short_name like '%AQR%' then 'INTERNAL_CLIENT'
-when (ac.NAME IN ('INVEST_ONE_CITIBANK', 'CITI_CUSTODY', 'VELOCITY_CITI')) then 'CITI'
-when short_name like '%VANGUARD%' or ac.name like '%Vanguard%' then 'INTERNAL_CLIENT'
-when ac.name in ('USBANKSPLIT', 'USBANKNOPRICE', 'USBANKCORP', 'USBANK MIDWOOD HOLDINGS', 'SEIINVESTMENT USBANKNEW TRANSACTIONS') then 'US BANK'
-when ac.name in ('SEI_SWP','SEI_BSD') then 'SEI_CLIENTS'
-when ac.name in ('PAULCO ENFUSION TRANSACTIONS REFRESH',  'Enfusion Data Upload Client') then 'ENFUSION'
-when ac.name in ('SEI_SWP','SEI_BSD') then 'SEI_CLIENTS'
-when ac.name in ( 'CITCO_PARTNER', 'Citco Fund Services') then 'CITCO'
-when  substr(ac.name,0,3) = 'SSC' or short_name = 'SCHONFELD' or ac.name = 'GENEVA' then 'SSC'
-when ac.name like '%BBH%' or ac.name like '%Bbh%' then 'BBH'
-when (ac.name like '%NORTHERN%' or ac.name like '%Northern%') then 'NORTHERN TRUST'
-when ((substr(ac.CLIENT_REF_NUMBER,0,2) = 'P ' or substr(ac.CLIENT_REF_NUMBER,0,2) = 'S '  and length(ac.CLIENT_REF_NUMBER) = 7)) or ((substr(ac.CLIENT_REF_NUMBER,0,1) = 'E' and length(ac.CLIENT_REF_NUMBER) = 5)) then 'JPM'
--- when (length(a.account_number) = 7  or length(a.account_number) = 9) and REGEXP_LIKE(a.account_number, '^[[:digit:]]+$') then 'BBH'
-when length(ac.CLIENT_REF_NUMBER) = 4 then 'SSB'
-when substr(ac.CLIENT_REF_NUMBER,4,1) = 'F' and substr(ac.CLIENT_REF_NUMBER,-1) = '2'  or ac.CLIENT_REF_NUMBER = 'AGGREGATE' then 'BNY'
--- when length(a.account_number) = 6   then 'BNY'  
-WHEN ac.NAME like ('%GSP%') or ac.name like '%NEXEN%' or ac.name like '%BNYM%' then 'BNY'
+case when ((ac.name like '%INTERNAL%' or ac.name like '%Internal%') and ac.name not in ('******')) or short_name like '%******%' then '******'
+when (ac.NAME IN ('******', '******', '******')) then '******'
+when short_name like '%******%' or ac.name like '%******%' then 'INTERNAL_CLIENT'
+when ac.name in ('******', '******', '******', '****** ****** ******', '****** ****** ******') then 'US ******'
+when ac.name in ('******','******') then '******'
+when ac.name in ('****** ****** TRANSACTIONS REFRESH',  '****** Data Upload Client') then '******'
+when ac.name in ('******','******') then '******'
+when ac.name in ( '******', '****** ****** Services') then '******'
+when  substr(ac.name,0,3) = '******' or short_name = '******' or ac.name = '******' then '******'
+when ac.name like '%******%' or ac.name like '%******%' then '******'
+when (ac.name like '%******%' or ac.name like '%******%') then '****** TRUST'
+when ((substr(ac.CLIENT_REF_NUMBER,0,2) = 'P ' or substr(ac.CLIENT_REF_NUMBER,0,2) = 'S '  and length(ac.CLIENT_REF_NUMBER) = 7)) or ((substr(ac.CLIENT_REF_NUMBER,0,1) = 'E' and length(ac.CLIENT_REF_NUMBER) = 5)) then '******'
+-- when (length(a.account_number) = 7  or length(a.account_number) = 9) and REGEXP_LIKE(a.account_number, '^[[:digit:]]+$') then '******'
+when length(ac.CLIENT_REF_NUMBER) = 4 then '******'
+when substr(ac.CLIENT_REF_NUMBER,4,1) = 'F' and substr(ac.CLIENT_REF_NUMBER,-1) = '2'  or ac.CLIENT_REF_NUMBER = 'AGGREGATE' then '******'
+-- when length(a.account_number) = 6   then '******'  
+WHEN ac.NAME like ('%******%') or ac.name like '%******%' or ac.name like '%******%' then '******'
 else 'INTERNAL_CLIENT_OR_UNKNOWN' end   
 as likely_data_source_name
 
@@ -440,7 +436,7 @@ WHERE AC.ACCOUNT_ID = A.ACCOUNT_ID
 )
 SELECT DISTINCT A.CLIENT_NAME,A.CLIENT_REF_NUMBER,B.LIKELY_DATA_SOURCE_NAME,COUNT (DISTINCT A.CASE_ID) AS "TOTAL_REQUESTS"
 FROM A,B
-WHERE A.CLIENT_NAME <> 'NEUBERGER'--- EXCLUDED FOR RUN TIME
+WHERE A.CLIENT_NAME <> '******'--- EXCLUDED FOR RUN TIME
 AND A.CLIENT_REF_NUMBER = B.CLIENT_REF_NUMBER
 GROUP BY A.CLIENT_NAME, A.CLIENT_REF_NUMBER,B.LIKELY_DATA_SOURCE_NAME
 ORDER BY TOTAL_REQUESTS DESC
@@ -606,7 +602,7 @@ from jbrown.jira_tasks JT,jbrown.JIRA_TASK_CASE_ID CI
 where project_name in ('Case Data Request')
 AND STATUS NOT IN ( 'Done')
 and created > SYSDATE - 30 -- 30 DAYS AGO
-AND (ASSIGNEE like '%Hoxha' or ASSIGNEE like '%Arroyo' or ASSIGNEE like'%Dubey' or ASSIGNEE like'%Siafrakas' or ASSIGNEE like'%Langin'  or ASSIGNEE like'%Tuite' or ASSIGNEE like'%Iascone' or ASSIGNEE like'%Roberts' or ASSIGNEE like'%Wasilewski' or ASSIGNEE like'%Firicano' or ASSIGNEE like'%Knight')
+AND (ASSIGNEE like '%******' or ASSIGNEE like '%******' or ASSIGNEE like'%******' or ASSIGNEE like'%******' or ASSIGNEE like'%******'  or ASSIGNEE like'%******' or ASSIGNEE like'%******' or ASSIGNEE like'%******' or ASSIGNEE like'%******' or ASSIGNEE like'%******' or ASSIGNEE like'%******')
 and JT.ID = CI.TASK_ID
 ;
 
@@ -619,7 +615,7 @@ from jbrown.jira_tasks JT,jbrown.JIRA_TASK_CASE_ID CI
 where project_name in ('Case Data Request')
 AND STATUS NOT IN ( 'Done')
 and created > SYSDATE - 30 -- 30 DAYS AGO
-AND (ASSIGNEE like '%Li' or ASSIGNEE like '%Xie' or ASSIGNEE like'%Varaiya' or ASSIGNEE like'%Boyd' or ASSIGNEE like'%Chen'  or ASSIGNEE like'%Meka' or ASSIGNEE like'%McGlashing')
+AND (ASSIGNEE like '%******' or ASSIGNEE like '%******' or ASSIGNEE like'%******' or ASSIGNEE like'%******' or ASSIGNEE like'%******'  or ASSIGNEE like'%******' or ASSIGNEE like'%******')
 and JT.ID = CI.TASK_ID
 ;
 
@@ -843,29 +839,7 @@ FROM CDRS_PAST_DUE, CDRS_ON_TIME
 
 
 
-------------------------------------------4. Open CDRs assigned to AM
 
-select key,case_id,summary,claim_deadline,assignee,status, due_date, round (sysdate - created) as "DAYS SINCE CREATION" 
-from jbrown.jira_tasks JT,jbrown.JIRA_TASK_CASE_ID CI
-where project_name in ('Case Data Request')
-AND STATUS NOT IN ( 'Done')
-and created > SYSDATE - 365 --  DAYS AGO
-AND (ASSIGNEE like '%Hoxha' or ASSIGNEE like '%Arroyo' or ASSIGNEE like'%Dubey' or ASSIGNEE like'%Siafrakas' or ASSIGNEE like'%Langin'  or ASSIGNEE like'%Tuite' or ASSIGNEE like'%Iascone' or ASSIGNEE like'%Roberts' or ASSIGNEE like'%Wasilewski' or ASSIGNEE like'%Firicano' or ASSIGNEE like'%Knight')
-and JT.ID = CI.TASK_ID
-;
-
-
-
------------------------------------------- 5. Open CDRs assigned to CDM
-
-select key,case_id,summary,claim_deadline,assignee,status, due_date, round (sysdate - created) as "DAYS SINCE CREATION" 
-from jbrown.jira_tasks JT,jbrown.JIRA_TASK_CASE_ID CI
-where project_name in ('Case Data Request')
-AND STATUS NOT IN ( 'Done')
-and created > SYSDATE - 365 -- DAYS AGO
-AND (ASSIGNEE like '%Li' or ASSIGNEE like '%Xie' or ASSIGNEE like'%Varaiya' or ASSIGNEE like'%Boyd' or ASSIGNEE like'%Chen'  or ASSIGNEE like'%Meka' or ASSIGNEE like'%McGlashing')
-and JT.ID = CI.TASK_ID
-;
 
 
 /*
